9f5397819774db3eb1d10684a1052d79e6d80f30
Thịnh Nguyễn Quốc <thinhnq3009@gmail.com>
2024-03-11 17:15:30 +0700

    Update using pymongo to connect DB

--- backend/app/app/db/mongo_db.py
+++ backend/app/app/db/mongo_db.py
@@ -1,7 +1,9 @@
 import motor.motor_asyncio
-
+from pymongo import MongoClient
 from app.core.config import settings
 
-client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGODB_URL)
-db = client.get_database("digital-twin")
+client = MongoClient(settings.MONGODB_URL)
+
+db = client["digital-twin"]
+
 users_collection = db.get_collection("users")
